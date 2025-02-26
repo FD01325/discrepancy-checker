44f55fd17c01ce60e0e34b415e9f79ea74780e17
FD01325 <kaushik.rao@fulcrumdigital.com>
2025-02-26 19:02:09 +0530

    Updated frontend code

--- src/main/java/com/mastercard/timesheet/discrepancy_checker/controller/TimesheetController.java
+++ src/main/java/com/mastercard/timesheet/discrepancy_checker/controller/TimesheetController.java
@@ -4,12 +4,10 @@ import com.mastercard.timesheet.discrepancy_checker.service.TimesheetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/timesheet")
 public class TimesheetController {
