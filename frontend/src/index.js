4657dacb1d4dab9660d3475fd2d7a1bb5b385fd3
FD01325 <kaushik.rao@fulcrumdigital.com>
2025-02-26 19:13:05 +0530

    Removed unnecessary frontend files

--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import "./index.css"; // Optional, for styling
-
-ReactDOM.render(<App />, document.getElementById('root'));
